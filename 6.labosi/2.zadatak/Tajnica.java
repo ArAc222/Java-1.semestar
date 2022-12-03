0a80148ecbd8d21aa96cb2d292e4cc1a7aea59d8
ArAc222 <105783863+ArAc222@users.noreply.github.com>
2022-12-03 18:27:33 +0100

    Create Tajnica.java

---
+++ 6.labosi/2.zadatak/Tajnica.java
@@ -0,0 +1,12 @@
+package labosi6;
+
+/**
+ *
+ * @author luka2
+ */
+public class Tajnica extends Zaposlenik{
+    @Override
+    public void radi(){
+        System.out.println("Zakazuje");
+    }
+}
