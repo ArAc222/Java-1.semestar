f79e6b52cf20ad38aa1503389ce044673d06dd99
ArAc222 <105783863+ArAc222@users.noreply.github.com>
2023-01-27 17:11:25 +0100

    Create Lastavica.java

---
+++ zadnji labosi/2.zadatak/Lastavica.java
@@ -0,0 +1,12 @@
+public class Lastavica extends Ptica {
+  String ime;
+  public void setIme(String ime){
+    this.ime=ime;
+  }
+  public String getIme(){
+    return ime;
+  }
+  public void glasajSe(){
+    System.out.println("Glasa se lastavica");
+  }
+}
