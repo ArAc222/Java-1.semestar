55bcf076a30901c167c64051192976b030ea6671
ArAc222 <105783863+ArAc222@users.noreply.github.com>
2023-01-27 17:09:44 +0100

    Create ptica.java

---
+++ zadnji labosi/2.zadatak/ptica.java
@@ -0,0 +1,15 @@
+public class Ptica implements Zivotinja {
+  
+  String ime;
+  
+  public void setIme(String ime){
+    this.ime=ime;
+  }
+  public String getIme(){
+    return ime;
+  }
+  public void glasajSe()
+  {
+    System.out.println("Glasa se ptica");
+  }
+}
