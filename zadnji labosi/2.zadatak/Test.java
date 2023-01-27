0c5cb65e002fe387420cf3836dd97e5740d5749d
ArAc222 <105783863+ArAc222@users.noreply.github.com>
2023-01-27 17:12:47 +0100

    Create Test.java

---
+++ zadnji labosi/2.zadatak/Test.java
@@ -0,0 +1,13 @@
+public class JavaApplication7 {
+  public static void main(String[] args) {
+    Lastavica objekt1 = new Lastavica();
+    Ptica objekt2 = new Ptica();
+    objekt1.setIme("Pero");
+    objekt1.getIme();
+    objekt1.glasajSe();
+    
+    objekt2.setIme("Vrabac");
+    objekt2.getIme();
+    objekt2.glasajSe();
+  }
+}
